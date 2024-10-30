3f5483385dcf8cc595c5c41089973a754dcbebf9
Caio Fernandes <133905605+caiolf9616@users.noreply.github.com>
2024-10-30 16:56:03 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,25 +1,9 @@
 
-public abstract class Pessoa {
+public class Pessoa {
     private String cpf;
     public String nome;
     public int idade;
     
-    
-    public Pessoa(String cpf, String nome, int idade){
-        this.cpf = cpf;
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-
-
-
-
-
-    
-
-    
-
     
 
     public String getCpf() {
